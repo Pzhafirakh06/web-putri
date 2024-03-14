b614dde09f79eec1418501cf85826bee0296f21e
unknown <you@example.com>
2024-03-14 15:48:04 +0700

    Variabel

---
+++ python/Registrasi.form.py
@@ -0,0 +1,4 @@
+name= input ("Enter Your Name?:")
+addres= (input("Enter Your addres ?"))
+email=(input("Enter Your email"))
+print(f"hello{name},{addres},{email}Registrasion succes")
---
+++ python/variabel.py
@@ -0,0 +1,23 @@
+firstname="Putri"
+lastname="Zhafirah"
+print(f"Hello {firstname}{lastname}")
+
+number1,number2,number3=21,10,99
+print(number1)
+print(number2)
+print(number3)
+
+fruits = ["mango","orange", "durian","Apple"]
+buah1,buah2,buah3,buah4 =fruits
+print(buah1)
+print(buah2)
+print(buah3)
+print(buah4)
+
+print('"Python is a programing language"') 
+print("Hello /n World")
+
+kata="lautan di api"
+print(kata.capitalize())
+print(kata.upper())
+print(kata.lower())
