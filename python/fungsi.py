e61926f50e37eed558e8feb90fe09a589a0f0fe0
unknown <you@example.com>
2024-04-29 09:55:36 +0700

    fungsi

---
+++ python/fungsi.py
@@ -0,0 +1,39 @@
+def myFunction():
+   print ("I Love Python")
+   
+myFunction()
+myFunction()
+myFunction()
+myFunction()
+
+def greeting():
+   print ("Selamat datang")
+greeting()
+
+def num1to100():
+      numbers = list(range(1,101))
+      print(numbers)
+num1to100()
+
+def sayHello (name,age):
+    print( f"Hello {name}, {age} years  old ")
+     
+sayHello("Ahmad",10)
+sayHello('Putri',17)
+
+def  buah(nama_buah):
+    print( f"nama {nama_buah} ")
+     
+def tambah(angka1,angka2):
+          return angka1 + angka2
+angka1 = 10
+angka2= 12
+
+hasil = tambah(angka1,angka2)
+
+result = hasil +10
+print(result)
+
+buah("Melon")
+buah("Semangka")
+
---
+++ python/simple-calculator-function.py
@@ -0,0 +1,9 @@
+def tambah (angka1,angka2):
+          return angka1 + angka2
+
+
+angka1 = float(input("Angka Pertama:"))
+angka2 = float(input("Angka Kedua:"))
+
+hasil_tambah = tambah(angka1,angka2)
+print (f"{angka1}  + {angka2} = {hasil_tambah}")
